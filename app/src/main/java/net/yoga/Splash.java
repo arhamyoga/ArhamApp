96300c834d55b171b9475f8fdf2ebecee58cda5d
pavan Kumar MVN <mvnpavan@gmail.com>
2017-11-12 12:21:51 +0530

    Initial commit : Base project with rough designs.

---
+++ app/src/main/java/net/yoga/Splash.java
@@ -0,0 +1,35 @@
+package net.yoga;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mvnpavan on 08/11/17.
+ */
+
+public class Splash extends AppCompatActivity {
+
+    private Context context;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        context = this;
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(context, MainActivity.class);
+                context.startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
