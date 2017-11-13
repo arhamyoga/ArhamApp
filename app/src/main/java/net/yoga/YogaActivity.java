94a7559bc315faca8a3cf3903fa24ed83f7a8f02
nayan <nayan@drishti-soft.com>
2017-11-13 14:56:40 +0530

    Resolution of video adjusted to work on Moto devices.

--- app/src/main/java/net/yoga/YogaActivity.java
+++ app/src/main/java/net/yoga/YogaActivity.java
@@ -1,10 +1,12 @@
 package net.yoga;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.VideoView;
@@ -33,6 +35,14 @@ public class YogaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yoga);
         videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Log.d("video", "setOnErrorListener ");
+                return true;
+            }
+        });
         progressView = (ArcProgress) findViewById(R.id.circleView);
         playButton = (ImageButton) findViewById(R.id.playView);
 
