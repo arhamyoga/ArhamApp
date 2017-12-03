a4a7ade5643b5c0e1aa51027d1bc5b2ddd10a16c
nayan <nayan@drishti-soft.com>
2017-12-03 05:46:57 +0530

    Included the pic, intro and mention of maharaj shri

--- app/src/main/java/net/yoga/MainActivity.java
+++ app/src/main/java/net/yoga/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Add in Oncreate() funtion after setContentView()*/
+        WebView webView = (WebView) findViewById(R.id.introWebView);
+        String path = "file:///android_asset/intro.html";
+        webView.loadUrl(path);
     }
 
     public void openYoga(View view) {
